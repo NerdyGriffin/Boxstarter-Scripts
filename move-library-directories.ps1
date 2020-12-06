3679a19b626e6502e46358a0465acac52d85e677
Christian Kunis <ckunis98@gmail.com>
2020-12-06 01:12:50 -0500

    Documents library is left default for desktop VM

--- move-library-directories.ps1
+++ move-library-directories.ps1
@@ -1,12 +1,6 @@
 try {
 	Disable-UAC
 
-	if (Test-Path 'D:\') {
-		Move-LibraryDirectory 'Personal' (Join-Path 'D:\Users' $env:Username 'Documents')
-		Create-SymLink -Path (Join-Path $env:UserProfile 'Documents') -Value (Join-Path 'D:\Users' $env:Username 'Documents') -ErrorAction SilentlyContinue
-		# Create-SymLink -Path "D:\Documents" -Value (Join-Path 'D:\Users' $env:Username 'Documents') -ErrorAction SilentlyContinue
-	}
-
 	if ((Get-ComputerName | Select-String 'DESKTOP') -And (Test-Path '\\GRIFFINUNRAID\media\')) {
 		Move-LibraryDirectory 'My Music' '\\GRIFFINUNRAID\media\Music' -ErrorAction SilentlyContinue
 		Create-SymLink -Path (Join-Path $env:UserProfile 'Music') -Value '\\GRIFFINUNRAID\media\Music' -ErrorAction SilentlyContinue
@@ -17,21 +11,25 @@ try {
 		Move-LibraryDirectory 'My Video' '\\GRIFFINUNRAID\media\Videos' -ErrorAction SilentlyContinue
 		Create-SymLink -Path (Join-Path $env:UserProfile 'Music') -Value '\\GRIFFINUNRAID\media\Videos' -ErrorAction SilentlyContinue
 	} elseif (Test-Path 'D:\') {
-		Move-LibraryDirectory 'My Music' (Join-Path 'D:\Users' $env:Username 'Music') -ErrorAction SilentlyContinue
+		Move-LibraryDirectory 'My Music' (Join-Path 'D:\Users' $env:Username 'Music')
 		Create-SymLink -Path (Join-Path $env:UserProfile 'Music') -Value (Join-Path 'D:\Users' $env:Username 'Music') -ErrorAction SilentlyContinue
 
-		Move-LibraryDirectory 'My Pictures' (Join-Path 'D:\Users' $env:Username 'Pictures') -ErrorAction SilentlyContinue
+		Move-LibraryDirectory 'My Pictures' (Join-Path 'D:\Users' $env:Username 'Pictures')
 		Create-SymLink -Path (Join-Path $env:UserProfile 'Pictures') -Value (Join-Path 'D:\Users' $env:Username 'Pictures') -ErrorAction SilentlyContinue
 
-		Move-LibraryDirectory 'My Video' (Join-Path 'D:\Users' $env:Username 'Videos') -ErrorAction SilentlyContinue
+		Move-LibraryDirectory 'My Video' (Join-Path 'D:\Users' $env:Username 'Videos')
 		Create-SymLink -Path (Join-Path $env:UserProfile 'Videos') -Value (Join-Path 'D:\Users' $env:Username 'Videos') -ErrorAction SilentlyContinue
 	}
 
 	if ((Get-ComputerName | Select-String 'DESKTOP') -And (Test-Path '\\GRIFFINUNRAID\personal\')) {
-		Move-LibraryDirectory 'Downloads' '\\GRIFFINUNRAID\personal\Downloads' -ErrorAction SilentlyContinue
+		Move-LibraryDirectory 'Downloads' '\\GRIFFINUNRAID\personal\Downloads'
 		Create-SymLink -Path (Join-Path $env:UserProfile 'Downloads') -Value '\\GRIFFINUNRAID\personal\Downloads' -ErrorAction SilentlyContinue
 	} elseif (Test-Path 'D:\') {
-		Move-LibraryDirectory 'Downloads' (Join-Path 'D:\Users' $env:Username 'Downloads') -ErrorAction SilentlyContinue
+		Move-LibraryDirectory 'Personal' (Join-Path 'D:\Users' $env:Username 'Documents')
+		Create-SymLink -Path (Join-Path $env:UserProfile 'Documents') -Value (Join-Path 'D:\Users' $env:Username 'Documents') -ErrorAction SilentlyContinue
+		# Create-SymLink -Path "D:\Documents" -Value (Join-Path 'D:\Users' $env:Username 'Documents') -ErrorAction SilentlyContinue
+
+		Move-LibraryDirectory 'Downloads' (Join-Path 'D:\Users' $env:Username 'Downloads')
 		Create-SymLink -Path (Join-Path $env:UserProfile 'Downloads') -Value (Join-Path 'D:\Users' $env:Username 'Downloads') -ErrorAction SilentlyContinue
 	}
 
