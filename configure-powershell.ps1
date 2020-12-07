db0b6a806f85b2a7d4029f08ce3a83d1ad6b7f79
Christian Kunis <ckunis98@gmail.com>
2020-12-07 18:04:53 -0500

    Added imports for Chocolatey and Boxstarter modules

--- configure-powershell.ps1
+++ configure-powershell.ps1
@@ -31,8 +31,8 @@ try {
 					'Set-Theme Paradox'
 				)
 				Write-Host >> $PROFILE # This will create the file if it does not already exist, otherwise it will leave the existing file unchanged
-				foreach ($ProfileString in $PowerlineProfile) {
-					if (-Not(Select-String -Pattern $ProfileString -Path $PROFILE )) {
+				if (-Not(Select-String -Pattern $PowerlineProfile[0] -Path $PROFILE )) {
+					foreach ($ProfileString in $PowerlineProfile) {
 						Write-Debug 'Attemping to add the following line to $PROFILE :' $ProfileString
 						Add-Content -Path $PROFILE -Value $ProfileString
 					}
@@ -72,8 +72,8 @@ try {
 					'Set-Alias -Name v -Value Get-ChildItem'
 				)
 				Write-Host >> $PROFILE # This will create the file if it does not already exist, otherwise it will leave the existing file unchanged
-				foreach ($ProfileString in $PSReadlineProfile) {
-					if (-Not(Select-String -Pattern $ProfileString -Path $PROFILE)) {
+				if (-Not(Select-String -Pattern $PSReadlineProfile[0] -Path $PROFILE)) {
+					foreach ($ProfileString in $PSReadlineProfile) {
 						Write-Debug 'Attemping to add the following line to $PROFILE :' $ProfileString
 						Add-Content -Path $PROFILE -Value $ProfileString
 					}
@@ -107,6 +107,71 @@ try {
 		Write-Debug ' See the log for details (' $Boxstarter.Log ').'
 		# Move on if Pipeworks install fails due to errors
 	}
+
+	#--- Import Chocolatey Modules
+	try {
+		$ScriptBlock = {
+			Write-Host 'Appending Configuration for Chocolatey to PowerShell Profile...'
+			$BoxstarterProfile = @(
+				'# Chocolatey profile',
+				'$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"',
+				'if (Test-Path($ChocolateyProfile)) {'
+				'	Import-Module "$ChocolateyProfile"'
+				'}'
+			)
+			Write-Host >> $PROFILE # This will create the file if it does not already exist, otherwise it will leave the existing file unchanged
+			if (-Not(Select-String -Pattern $BoxstarterProfile[0] -Path $PROFILE)) {
+				foreach ($ProfileString in $BoxstarterProfile) {
+					Write-Debug 'Attemping to add the following line to $PROFILE :' $ProfileString
+					Add-Content -Path $PROFILE -Value $ProfileString
+				}
+			}
+		}
+		# Add the lines to the $PROFILE for PowerShell
+		powershell.exe -Command $ScriptBlock
+		# Do the same for PowerShell Core
+		pwsh.exe -Command $ScriptBlock
+	} catch {
+		Write-Warning 'Something went wrong while trying to configure $PROFILE for PSReadline.'
+		Write-Debug ' See the log for details (' $Boxstarter.Log ').'
+	}
+
+
+	#--- Import Boxstarter Modules
+	try {
+		$ScriptBlock = {
+			Write-Host 'Appending Configuration for Boxstarter to PowerShell Profile...'
+			$BoxstarterProfile = @(
+				'# Boxstarter modules',
+				'# Import the Chocolatey module first so that $Boxstarter properties',
+				'# are initialized correctly and then import everything else.',
+				'if (Test-Path("\\GRIFFINUNRAID\Boxstarter")) {',
+				'	$BoxstarterInstall = "\\GRIFFINUNRAID\Boxstarter"',
+				'} elseif (Test-Path("D:\Boxstarter")) {',
+				'	$BoxstarterInstall = "D:\Boxstarter"',
+				'}',
+				'Import-Module $BoxstarterInstall\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1 -DisableNameChecking -ErrorAction SilentlyContinue',
+				'Resolve-Path $BoxstarterInstall\Boxstarter.*\*.psd1 |',
+				'	% { Import-Module $_.ProviderPath -DisableNameChecking -ErrorAction SilentlyContinue }',
+				'Import-Module $BoxstarterInstall\Boxstarter.Common\Boxstarter.Common.psd1 -Function Test-Admin'
+			)
+			Write-Host >> $PROFILE # This will create the file if it does not already exist, otherwise it will leave the existing file unchanged
+			if (-Not(Select-String -Pattern $BoxstarterProfile[0] -Path $PROFILE)) {
+				foreach ($ProfileString in $BoxstarterProfile) {
+					Write-Debug 'Attemping to add the following line to $PROFILE :' $ProfileString
+					Add-Content -Path $PROFILE -Value $ProfileString
+				}
+			}
+		}
+		# Add the lines to the $PROFILE for PowerShell
+		powershell.exe -Command $ScriptBlock
+		# Do the same for PowerShell Core
+		pwsh.exe -Command $ScriptBlock
+	} catch {
+		Write-Warning 'Something went wrong while trying to configure $PROFILE for PSReadline.'
+		Write-Debug ' See the log for details (' $Boxstarter.Log ').'
+	}
+
 
 	Enable-UAC
 	Enable-MicrosoftUpdate
