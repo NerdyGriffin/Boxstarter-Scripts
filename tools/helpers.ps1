b2c66744b1dc454f15cd21e1b5b8062c9d7c8c43
Christian Kunis <ckunis98@gmail.com>
2020-12-07 18:03:55 -0500

    Removed custom directory mover that is no longer needed

--- tools/helpers.ps1
+++ tools/helpers.ps1
@@ -1,34 +1,3 @@
-Function MergeMove-Directory {
-	[Parameter(Mandatory)]
-	[ValidateNotNullOrEmpty()]
-	[string]$Path,
-
-	[Parameter(Mandatory)]
-	[ValidateNotNullOrEmpty()]
-	[string]$Destination
-
-	if (Test-Path $Path) {
-		if (Get-Item $Path | Where-Object Attributes -Match ReparsePoint) {
-			Write-Warning $Path 'Is already a reparse point.'
-			Return $false
-		}
-		if (Test-Path $Destination) {
-			Get-ChildItem $Path | ForEach-Object -Process {
-				Copy-Item -Path (Join-Path $Path $_) -Destination $Destination -Force -Recurse -Verbose
-			}
-			Remove-Item -Path $Path -Recurse -Force -Verbose -ErrorAction Stop
-			Return $true
-		} else {
-			Return (Move-Item -Path $Path -Destination $Destination -Force -PassThru -Verbose -ErrorAction Stop)
-		}
-	} elseif (-Not(Test-Path $Destination)) {
-		Return (New-Item -Path $Destination -Type Directory -Force -Verbose -ErrorAction Stop)
-	} else {
-		Return (Get-Item -Path $Destination)
-	}
-	Return $false
-}
-
 Function New-SymLink {
 	param(
 		# Specifies the path of the location of the new link. You must include the name of the new link in Path .
