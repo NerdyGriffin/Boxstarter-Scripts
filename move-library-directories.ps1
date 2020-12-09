443d3fd459b1354a9d5eed173e5c189a7e619499
Christian Kunis <ckunis98@gmail.com>
2020-12-09 01:16:42 -0500

    Copied function 'New-SymLink' directly into the necessary functions because I don't have time to debug right now

--- move-library-directories.ps1
+++ move-library-directories.ps1
@@ -1,3 +1,42 @@
+Function New-SymLink {
+	param(
+		# Specifies the path of the location of the new link. You must include the name of the new link in Path .
+		[Parameter(Mandatory = $true,
+			Position = 0,
+			ParameterSetName = 'ParameterSetName',
+			ValueFromPipeline = $true,
+			ValueFromPipelineByPropertyName = $true,
+			HelpMessage = 'Specifies the path of the location of the new link. You must include the name of the new link in Path .')]
+		[Alias('PSPath')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$Path,
+
+		# Specifies the path of the location that you would like the link to point to.
+		[Parameter(Mandatory = $true,
+			Position = 1,
+			HelpMessage = 'Specifies the path of the location that you would like the link to point to.')]
+		[Alias('Target')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$Value
+	)
+
+	if ((Test-Path $Path) -And (Get-Item $Path | Where-Object Attributes -Match ReparsePoint)) {
+		Write-Warning $Path 'is already a reparse point.'
+		Return $false
+	} elseif (Test-Path "$Path\*") {
+		# $MoveResult = (Move-Item -Path $Path\* -Destination $Destination -Force -PassThru -Verbose -ErrorAction Stop)
+		$MoveResult = (robocopy $Path $Destination /ZB /FFT)
+		if (-Not($MoveResult)) {
+			Write-Warning 'Something went wrong while trying to move the contents of' $Path 'to' $Value
+			Return $MoveResult
+		}
+		Remove-Item -Path $Path\* -Recurse -Force -ErrorAction Inquire
+	}
+	Return (New-Item -Path $Path -ItemType SymbolicLink -Value $Value -Force -Verbose -ErrorAction Stop)
+}
+
 try {
 	Disable-UAC
 
