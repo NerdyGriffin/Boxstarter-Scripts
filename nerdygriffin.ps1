99a73c71177bcdd924dbec4190c4768557ab2601
Christian Kunis <ckunis98@gmail.com>
2020-12-26 16:03:04 -0500

    Added signal to the default installer

--- nerdygriffin.ps1
+++ nerdygriffin.ps1
@@ -44,6 +44,7 @@ try {
 	#--- Communication ---
 	choco install -y discord
 	choco install -y jitsi-meet-electron
+	choco install -y signal
 	choco install -y slack
 	choco install -y telegram
 	choco install -y zoom
