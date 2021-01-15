21e0f0f89cbd5c8778938b0c8a8ddfe77ed4132a
Christian Kunis <ckunis98@gmail.com>
2021-01-15 16:53:00 -0500

    Removed package 'findutils' due to Cmdlet name conflict with VSCode

--- windows-dev-tools.ps1
+++ windows-dev-tools.ps1
@@ -97,7 +97,7 @@ try {
 	#--- GNU ---
 	choco install -y awk
 	choco install -y diffutils
-	choco install -y findutils
+	# choco install -y findutils # Conflicts with command 'code' for VSCode
 	choco install -y gimp
 	choco install -y gnuplot
 	choco install -y gnuwin
