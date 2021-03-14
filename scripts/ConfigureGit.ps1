0c9192f25bb6551dce932b047a1ccd65f250e877
Christian Kunis <ckunis98@gmail.com>
2021-03-14 18:02:27 -0400

    Moved git and symlinks to a separate file

---
+++ scripts/ConfigureGit.ps1
@@ -0,0 +1,35 @@
+if (Get-Command choco -ErrorAction SilentlyContinue) {
+	choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+	choco install -y Gpg4win
+	refreshenv
+}
+
+#--- Configure Git ---
+git config --global user.name 'Christian Kunis'
+git config --global user.email 'ckunis98@gmail.com'
+if (Get-Command nano -ErrorAction SilentlyContinue) {
+	git config --global core.editor nano
+} else {
+	git config --global core.editor code
+}
+git config --global core.symlinks true
+git config --global core.autocrlf input
+git config --global core.eol lf
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+git config --global color.interactive auto
+git config --global color.ui true
+git config --global color.pager true
+git config --global color.showbranch auto
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.ft fetch
+git config --global alias.ps push
+git config --global alias.ph push
+git config --global alias.pl pull
+if (Test-Path 'C:\Program Files (x86)\GnuPG\bin\gpg.exe') {
+	git config --global gpg.program 'C:\Program Files (x86)\GnuPG\bin\gpg.exe'
+}
---
+++ scripts/GameSymlinks.ps1
@@ -0,0 +1,66 @@
+Function New-SymbolicLink {
+	param(
+		# Specifies the path of the location of the new link. You must include the name of the new link in Path .
+		[Parameter(Mandatory = $true,
+			Position = 0,
+			ParameterSetName = 'ParameterSetName',
+			ValueFromPipeline = $true,
+			ValueFromPipelineByPropertyName = $true,
+			HelpMessage = 'Specifies the path of the location of the new link. You must include the name of the new link in Path .')]
+		[Alias('PSPath')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$Path,
+
+		# Specifies the path of the location that you would like the link to point to.
+		[Parameter(Mandatory = $true,
+			Position = 1,
+			HelpMessage = 'Specifies the path of the location that you would like the link to point to.')]
+		[Alias('Target')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$Value
+	)
+
+	if ((Test-Path $Path) -And (Get-Item $Path | Where-Object Attributes -Match ReparsePoint)) {
+		Write-Host  $Path 'is already a reparse point.' | Write-Warning
+		Return $false
+	}
+	if (Test-Path "$Path\*") {
+		# $MoveResult = (Move-Item -Path $Path\* -Destination $Value -Force -PassThru -Verbose)
+		$MoveResult = (robocopy $Path $Value /ZB /FFT)
+		if (-Not($MoveResult)) {
+			Write-Host  'Something went wrong while trying to move the contents of' $Path 'to' $Value | Write-Warning
+			Return $MoveResult
+		}
+		Remove-Item -Path $Path\* -Recurse -Force -ErrorAction Inquire
+	}
+	if (Test-Path $Path) {
+		Remove-Item $Path -Recurse -Force
+	}
+	if (-Not(Test-Path $Value)) {
+		New-Item -Path $Value -ItemType Directory
+	}
+	$Result = New-Item -Path $Path -ItemType SymbolicLink -Value $Value -Force -Verbose
+	if ($Result) {
+		Write-Host  'Successfully created SymLink at' $Path 'pointing to' $Value | Write-Verbose
+		Return $true
+	} else {
+		Write-Host 'The following error occured while trying to make symlink: ' $Result | Write-Warning
+		Return $false
+	}
+}
+
+if (-Not(Get-Command New-SymbolicLink)) {
+	Write-Error "The 'New-SymbolicLink' helper function was not found."
+	throw
+}
+
+if (Test-Path 'D:\') {
+	# SymbolicLinks in AppData
+	$SymbolicLinkNames = @('Citra')
+	if ($env:USERDOMAIN | Select-String 'LAPTOP') { $SymbolicLinkNames += @('.minecraft') }
+	foreach ($FolderName in $SymbolicLinkNames) {
+		New-SymbolicLink -Path (Join-Path $env:APPDATA $FolderName) -Value (Join-Path 'D:\' $FolderName)
+	}
+}
