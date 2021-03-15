86dd25d256ecfb4d3986c80e403d2c676835e26f
Christian Kunis <ckunis98@gmail.com>
2021-03-15 09:29:54 -0400

    Added ssh-copy-id

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -3,6 +3,7 @@
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
 choco install -y gitkraken
+choco install -y ssh-copy-id
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
