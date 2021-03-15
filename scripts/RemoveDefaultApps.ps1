33dbf2926efe500336d22aae7b262f10ba586ef5
Christian Kunis <ckunis98@gmail.com>
2021-03-14 22:31:53 -0400

    Customize excluded apps

--- scripts/RemoveDefaultApps.ps1
+++ scripts/RemoveDefaultApps.ps1
@@ -1,5 +1,5 @@
 #--- Uninstall unnecessary applications that come with Windows out of the box ---
-Write-Host "Uninstall some applications that come with Windows out of the box" -ForegroundColor "Yellow"
+Write-Host 'Uninstall some applications that come with Windows out of the box' -ForegroundColor 'Yellow'
 
 #Referenced to build script
 # https://docs.microsoft.com/en-us/windows/application-management/remove-provisioned-apps-during-update
@@ -12,53 +12,53 @@ function removeApp {
 	Param ([string]$appName)
 	Write-Output "Trying to remove $appName"
 	Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
-	Get-AppXProvisionedPackage -Online | Where DisplayName -like $appName | Remove-AppxProvisionedPackage -Online
+	Get-AppxProvisionedPackage -Online | Where-Object DisplayName -Like $appName | Remove-AppxProvisionedPackage -Online
 }
 
 $applicationList = @(
-	"Microsoft.BingFinance"
-	"Microsoft.3DBuilder"
-	"Microsoft.BingFinance"
-	"Microsoft.BingNews"
-	"Microsoft.BingSports"
-	"Microsoft.BingWeather"
-	"Microsoft.CommsPhone"
-	"Microsoft.Getstarted"
-	"Microsoft.WindowsMaps"
-	"*MarchofEmpires*"
-	"Microsoft.GetHelp"
-	"Microsoft.Messaging"
-	"*Minecraft*"
-	"Microsoft.MicrosoftOfficeHub"
-	"Microsoft.OneConnect"
-	"Microsoft.WindowsPhone"
-	"Microsoft.WindowsSoundRecorder"
-	"*Solitaire*"
-	"Microsoft.MicrosoftStickyNotes"
-	"Microsoft.Office.Sway"
-	"Microsoft.XboxApp"
-	"Microsoft.XboxIdentityProvider"
-	"Microsoft.ZuneMusic"
-	"Microsoft.ZuneVideo"
-	"Microsoft.NetworkSpeedTest"
-	"Microsoft.FreshPaint"
-	"Microsoft.Print3D"
-	"*Autodesk*"
-	"*BubbleWitch*"
-    "king.com*"
-    "G5*"
-	"*Dell*"
-	"*Facebook*"
-	"*Keeper*"
-	"*Netflix*"
-	"*Twitter*"
-	"*Plex*"
-	"*.Duolingo-LearnLanguagesforFree"
-	"*.EclipseManager"
-	"ActiproSoftwareLLC.562882FEEB491" # Code Writer
-	"*.AdobePhotoshopExpress"
+	'Microsoft.BingFinance'
+	# "Microsoft.3DBuilder"
+	'Microsoft.BingFinance'
+	'Microsoft.BingNews'
+	'Microsoft.BingSports'
+	# "Microsoft.BingWeather"
+	'Microsoft.CommsPhone'
+	# "Microsoft.Getstarted"
+	'Microsoft.WindowsMaps'
+	'*MarchofEmpires*'
+	# "Microsoft.GetHelp"
+	# "Microsoft.Messaging"
+	# "*Minecraft*"
+	# "Microsoft.MicrosoftOfficeHub"
+	# "Microsoft.OneConnect"
+	'Microsoft.WindowsPhone'
+	'Microsoft.WindowsSoundRecorder'
+	'*Solitaire*'
+	# "Microsoft.MicrosoftStickyNotes"
+	'Microsoft.Office.Sway'
+	# "Microsoft.XboxApp"
+	# "Microsoft.XboxIdentityProvider"
+	# "Microsoft.ZuneMusic"
+	# "Microsoft.ZuneVideo"
+	'Microsoft.NetworkSpeedTest'
+	'Microsoft.FreshPaint'
+	# "Microsoft.Print3D"
+	# "*Autodesk*"
+	'*BubbleWitch*'
+	'king.com*'
+	'G5*'
+	'*Dell*'
+	'*Facebook*'
+	'*Keeper*'
+	'*Netflix*'
+	'*Twitter*'
+	# "*Plex*"
+	# "*.Duolingo-LearnLanguagesforFree"
+	'*.EclipseManager'
+	'ActiproSoftwareLLC.562882FEEB491' # Code Writer
+	'*.AdobePhotoshopExpress'
 );
 
 foreach ($app in $applicationList) {
-    removeApp $app
+	removeApp $app
 }
