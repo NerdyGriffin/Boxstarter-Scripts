0fcfc238a7913610476453a1ef824aac8b69c32b
Christian Kunis <ckunis98@gmail.com>
2021-03-15 09:31:16 -0400

    Updated UWP Sample script to use my preferred github dir

--- scripts/GetUwpSamplesOffGithub.ps1
+++ scripts/GetUwpSamplesOffGithub.ps1
@@ -1,5 +1,8 @@
 Update-SessionEnvironment
-Set-Location $env:USERPROFILE\desktop
+if (Test-Path 'C:\Program Files (x86)\GnuPG\bin\gpg.exe') {
+	git config --global gpg.program 'C:\Program Files (x86)\GnuPG\bin\gpg.exe'
+}
+Set-Location C:\Github
 mkdir UwpSamples
 Set-Location UwpSamples
 git clone https://github.com/Microsoft/Windows-universal-samples/
