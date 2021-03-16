077cdf1c91e1b4ac4f8893c329f57eb28aafafbc
Christian Kunis <ckunis98@gmail.com>
2021-03-15 22:14:25 -0400

    Added dependency for wireshark

--- scripts/NetworkTools.ps1
+++ scripts/NetworkTools.ps1
@@ -1,4 +1,5 @@
 #--- Advanced Network Tools ---
 # choco install -y advanced-ip-scanner # Possibly broken for FOSS users
 # choco install -y ipscan # Possibly broken for FOSS users
+choco install -y winpcap # Dependency for wireshark
 choco install -y wireshark
