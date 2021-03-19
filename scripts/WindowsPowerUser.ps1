34b8fe9979191a238089dc1715ef457c3a4e192e
Christian Kunis <ckunis98@gmail.com>
2021-03-19 00:39:13 -0400

    Added commands to make OneDrive startup after Sharex

--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -9,3 +9,12 @@ choco install -y shutup10
 choco install -y sharex
 choco install -y winaero-tweaker
 # choco install -y xyplorer
+
+# Add a custom registry entry for running Sharex at startup
+# $SharexCommand = """$env:ProgramFiles\Sharex\sharex.exe"""
+# Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
+
+# Modify the OneDrive startup entry to delay, so that it starts after Sharex (to prevent OneDrive from hooking the print hotkey)
+$CurrentOneDriveCommand = Get-ItemPropertyValue -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name OneDrive;
+$NewOneDriveCommand = "sleep 300; $CurrentOneDriveCommand";
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name OneDrive -Value $NewOneDriveCommand;
