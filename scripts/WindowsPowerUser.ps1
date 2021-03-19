959307074bc8a1683f7e05fe3e725dcc1f0bec1d
Christian Kunis <ckunis98@gmail.com>
2021-03-19 16:34:19 -0400

    Commented out Winaero because I dont really use it anymore

--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -7,7 +7,7 @@ choco install -y plasso --ignore-checksums # The checksums are never correct on 
 choco install -y reshack
 choco install -y shutup10
 choco install -y sharex
-choco install -y winaero-tweaker
+# choco install -y winaero-tweaker
 # choco install -y xyplorer
 
 # Add a custom registry entry for running Sharex at startup
