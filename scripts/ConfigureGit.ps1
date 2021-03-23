44723f60d249bc34fe16d01b6e7ea748420d0a20
Christian Kunis <ckunis98@gmail.com>
2021-03-22 22:12:24 -0400

    Saving files before refreshing line endings

--- scripts/ConfigureGit.ps1
+++ scripts/ConfigureGit.ps1
@@ -12,7 +12,7 @@ if (Get-Command nano -ErrorAction SilentlyContinue) {
 	git config --global core.editor code
 }
 git config --global core.symlinks true
-# git config --global core.autocrlf input
+git config --global core.autocrlf true
 git config --global color.status auto
 git config --global color.diff auto
 git config --global color.branch auto
