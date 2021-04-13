c1cf38a9e9e78d988e24aeec964cea41f7d7cfb5
Christian Kunis <ckunis98@gmail.com>
2021-04-13 14:39:49 -0400

    Added startup reg entry for PowerToys, and added better explanation to Sharex startup

--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -10,11 +10,13 @@ choco install -y sharex
 # choco install -y winaero-tweaker
 # choco install -y xyplorer
 
+# Add a custom registry entry for running PowerToys at startup
+$PowerToysCommand = """$env:ProgramFiles\PowerToys\PowerToys.exe"""
+Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name PowerToys -Value $PowerToysCommand
+
 # Add a custom registry entry for running Sharex at startup
 $SharexCommand = """$env:ProgramFiles\Sharex\sharex.exe"""
 Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
-
-# # Modify the OneDrive startup entry to delay, so that it starts after Sharex (to prevent OneDrive from hooking the print hotkey)
-# $CurrentOneDriveCommand = Get-ItemPropertyValue -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name OneDrive;
-# $NewOneDriveCommand = "$CurrentOneDriveCommand";
-# Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name OneDrive -Value $NewOneDriveCommand;
+# We want Sharex to always startup BEFORE OneDrive (to prevent OneDrive from hooking the print hotkey)
+# This Sharex entry is added to 'HKLM:\...\Run' whereas the OneDrive startup entry is in 'HKCU:\...\Run' by default,
+# Therefore we know that Sharex will always be started before OneDrive (because Windows always parses entries in 'HKLM:' first)
