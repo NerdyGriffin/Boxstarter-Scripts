70df6afc7fb090ac1130c7b9f639007a0d4f03d5
Christian Kunis <ckunis98@gmail.com>
2021-04-16 20:27:16 -0400

    Added standalone installer for OpenSSH

---
+++ openssh.ps1
@@ -0,0 +1,34 @@
+$Boxstarter.StopOnPackageFailure = $false
+
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = '-bootstrapPackage'
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", ' ')
+$helperUri = $helperUri.TrimEnd("'", ' ')
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf('/'))
+$helperUri += '/scripts'
+Write-Host "helper script base URI is $helperUri"
+
+function executeScript {
+	Param ([string]$script)
+	Write-Host "executing $helperUri/$script ..."
+	Invoke-Expression ((New-Object net.webclient).DownloadString("$helperUri/$script")) -ErrorAction Continue
+	RefreshEnv;
+	Start-Sleep -Seconds 1;
+}
+
+#--- SSH Server ---
+executeScript 'OpenSSHServer.ps1';
+
+Get-Content -Path $Boxstarter.Log | Select-String -Pattern '^Failures$' -Context 0, 2 >> (Join-Path $env:USERPROFILE 'Desktop\boxstarter-failures.log')
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
+
+Write-Output 'Please restart your computer after exiting this script'
