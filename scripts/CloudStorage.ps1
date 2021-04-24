f7844bce4e8a3316253a490a41bd7b025774faf6
Christian Kunis <ckunis98@gmail.com>
2021-04-24 02:00:16 -0400

    Added a lazy makeshift semaphore to prevent a reboot loop

--- scripts/CloudStorage.ps1
+++ scripts/CloudStorage.ps1
@@ -1,3 +1,10 @@
 choco install -y google-backup-and-sync
 # choco install -y megasync
-choco install -y nextcloud-client
+
+$NextcloudPlaceholder = Join-Path (Get-LibraryNames).Desktop 'nextcloud-placeholder'
+if (Test-Path $NextcloudPlaceholder) {
+	Remove-Item -Path $NextcloudPlaceholder
+} else {
+	New-Item -Path $NextcloudPlaceholder -ItemType File -Value 'This is just a marker for the script. You may delete this file'
+	choco install -y nextcloud-client
+}
