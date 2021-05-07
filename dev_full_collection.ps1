88791c9aacb324c12862c351024bfe3dfc15a6af
Christian Kunis <ckunis98@gmail.com>
2021-05-06 22:05:25 -0400

    Changed to use new git cloner script

--- dev_full_collection.ps1
+++ dev_full_collection.ps1
@@ -124,14 +124,7 @@ try {
 }
 
 # checkout recent projects
-Set-Location C:\Github
-git clone https://github.com/microsoft/windows-dev-box-setup-scripts
-git clone https://github.com/microsoft/winappdriver
-git clone https://github.com/microsoft/wsl
-git clone https://github.com/PowerShell/PowerShell
-git clone https://github.com/NerdyGriffin/Boxstarter-Scripts
-git clone https://github.com/gordon-cs/gordon-360-ui
-git clone https://github.com/gordon-cs/gordon-360-api
+executeScript 'GetFavoriteProjects.ps1'
 
 executeScript 'GetUwpSamplesOffGithub.ps1';
 executeScript 'WindowsTemplateStudio.ps1';
