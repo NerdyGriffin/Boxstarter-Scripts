724ebbf55abc96b7d4f9f5ca538528790286241b
Christian Kunis <ckunis98@gmail.com>
2021-06-04 13:47:29 -0400

    Added additional tools for arduino

--- scripts/MiscDevTools.ps1
+++ scripts/MiscDevTools.ps1
@@ -1,10 +1,13 @@
 #--- Assorted Dev Tools and Dependencies
+choco install -y HexEdit
 choco install -y androidstudio
 choco install -y arduino
+choco install -y arduino-cli
 choco install -y electron
 choco install -y fiddler
-choco install -y HexEdit
+choco install -y fritzing
 choco install -y hxd
 choco install -y meld
 choco install -y ngrok
 choco install -y notepadplusplus
+choco install -y vscode-arduino
