fa0c70d2e728fab22ba90c3e483c72af0588976e
Christian Kunis <ckunis98@gmail.com>
2021-06-21 15:53:11 -0400

    Added chocolatey packages to list of github repos

--- scripts/GetFavoriteProjects.ps1
+++ scripts/GetFavoriteProjects.ps1
@@ -4,6 +4,7 @@ Set-Location C:\Github
 # Feel free to customize this with your own preferred projects
 git clone https://github.com/NerdyGriffin/Boxstarter-Scripts
 git clone https://github.com/PowerShell/PowerShell
+git clone https://github.com/chocolatey-community/chocolatey-coreteampackages.git
 git clone https://github.com/gordon-cs/gordon-360-api
 git clone https://github.com/gordon-cs/gordon-360-ui
 git clone https://github.com/microsoft/winappdriver
