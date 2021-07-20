b63a6ad4bc886473370e89add602891460200a05
Christian Kunis <ckunis98@gmail.com>
2021-07-19 20:31:34 -0400

    Added latencymon for testing CPU latency on VM

--- scripts/HardwareMonitoring.ps1
+++ scripts/HardwareMonitoring.ps1
@@ -3,5 +3,6 @@ choco install -y cpu-z
 choco install -y gpu-z
 choco install -y hwinfo
 choco install -y hwmonitor
+choco install -y latencymon
 choco install -y msiafterburner
 choco install -y msiafterburner --ignore-checksums
