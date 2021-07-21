d7252e096c6a7393a867ac05366ed6d4458b8ae9
Christian Kunis <ckunis98@gmail.com>
2021-07-20 22:46:40 -0400

    Temporarily remove WSL features for Windows 11 testing

--- dev_full_collection.ps1
+++ dev_full_collection.ps1
@@ -107,21 +107,21 @@ executeScript 'OpenJDK.ps1';
 executeScript 'GetMLIDEAndTooling.ps1';
 executeScript 'PythonMLTools.ps1';
 
-executeScript 'HyperV.ps1';
-executeScript 'WSL.ps1';
+# executeScript 'HyperV.ps1';
+# executeScript 'WSL.ps1';
 
-try {
-	Write-Host 'Installing tools inside the WSL distro...'
-	Ubuntu1804 run apt install neofetch -y
-	Ubuntu1804 run apt install nodejs -y
-	Ubuntu1804 run apt install git-core -y
-	Ubuntu1804 run apt install git-extras -y
-	Ubuntu1804 run apt install python2.7 python-pip -y
-	Ubuntu1804 run apt install python-numpy python-scipy -y
-	Ubuntu1804 run pip install pandas
-} catch {
-	# Skip for now
-}
+# try {
+# 	Write-Host 'Installing tools inside the WSL distro...'
+# 	Ubuntu1804 run apt install neofetch -y
+# 	Ubuntu1804 run apt install nodejs -y
+# 	Ubuntu1804 run apt install git-core -y
+# 	Ubuntu1804 run apt install git-extras -y
+# 	Ubuntu1804 run apt install python2.7 python-pip -y
+# 	Ubuntu1804 run apt install python-numpy python-scipy -y
+# 	Ubuntu1804 run pip install pandas
+# } catch {
+# 	# Skip for now
+# }
 
 # checkout recent projects
 executeScript 'GetFavoriteProjects.ps1'
